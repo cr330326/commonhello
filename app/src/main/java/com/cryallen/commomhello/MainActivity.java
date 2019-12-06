2fc18cac3295fa1810ac2d70917ad9c517d5c3b5
Allen <cr330326@gmail.com>
2019-12-06 10:21:26 +0800

    Add: 增加测试log代码

--- app/src/main/java/com/cryallen/commomhello/MainActivity.java
+++ app/src/main/java/com/cryallen/commomhello/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.cryallen.commonlib.utils.LogUtils;
+
 public class MainActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		LogUtils.setDebuggable(true);
+		LogUtils.d("MainActivity","test122323");
 	}
 }
